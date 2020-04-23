90033053f39cb5f19b42f93ee1141e3771db9ca8
abbyhu2000 <abigailhu2000@gmail.com>
2020-04-23 17:13:22 -0500

    Finished project

---
+++ main.cpp
@@ -0,0 +1,83 @@
+// Sample driver
+// EE 312 Fall 2018
+//
+// The BST should work for any data type or object.
+// Other user created objects must have the << operator overloaded for
+// output.
+//
+
+#include <iostream>
+#include <fstream>
+#include "BST312.h"
+
+using namespace std;
+
+template <typename T>
+void print(vector<T> vec){
+    for(int i = 0 ; i < vec.size() ; i++){
+        cout << vec[i] << endl;;
+    }
+}
+
+int main() {
+
+    ifstream inFile;
+    inFile.open("test.txt");
+
+    BST_312<string> bst;
+
+
+    string s;
+    inFile >> s;
+    while (inFile) {;
+        cout << "inserting ... " << s << endl;
+        bst.insertItem(s);
+        inFile >> s;
+    }
+
+//    BST_312<int> bst;
+//    int s;
+//    inFile >> s;
+//    while(inFile){
+//        cout << "inserting ... " << s << endl;
+//        bst.insertItem(s);
+//        inFile >> s;
+//    }
+
+
+    cout << "postorder traversal is " << endl;
+    print(bst.postOrderTraversal());
+    cout << endl;
+
+    cout << "preorder traversal is " << endl;
+    print(bst.preOrderTraversal());
+    cout << endl;
+
+    cout << "inorder traversal is " << endl;
+    print(bst.inOrderTraversal());
+    cout << endl;
+
+    cout << "Remove items " << endl;
+    cout << "number of nodes in tree before delete is " << bst.countNodes() << endl;
+    s = "tree";
+    bst.deleteItem(s);
+    s = "la";
+    bst.deleteItem(s);
+    print(bst.postOrderTraversal());
+    cout << endl;
+    cout << "number of nodes in tree after delete is " << bst.countNodes() << endl;
+    cout << endl;
+
+    cout << "Is tree in the tree? " << bst.isItemInTree("tree") << endl;
+    cout << "Is la in the tree? " << bst.isItemInTree("la") << endl;
+    cout << "Is tre in the tree? " << bst.isItemInTree("tre") << endl;
+    cout << "Is series in the tree? " << bst.isItemInTree("series") << endl;
+    cout << "Is This in the tree? " << bst.isItemInTree("This") << endl;
+
+    bst.makeEmpty();
+    print(bst.postOrderTraversal());
+    cout << "number of nodes in tree after empty is " << bst.countNodes() << endl;
+    cout << "Is This in the tree? " << bst.isItemInTree("This") << endl;
+
+}
+
